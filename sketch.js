ed65b4aae175c05a64906f813b30ffd8a88c3160
Sergey Litvinov <slitvinov@gmail.com>
2025-07-25 10:48:39 +0200

    sketch

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
-t=0
-draw=$ => {
+function setup () {
     w = 400
-    t || createCanvas(w, w);
-    background(9).stroke(w, 96);
-    for(t += PI/240, i = 1e4; i--;) {
-	x = i
-	y = i / 235
-	k = (4+sin(x/11+t*8))*cos(x/14)
-	e = y/8-19
-	d = mag(k, e) + sin(y/9 + t*2)
-	point((q=2*sin(k*2)+sin(y/17)*k*(9+2*sin(y-d*3)))+50*cos(c=d*d/49-t)+200,
-	      q*sin(c)+d*39-440)
+    t = 0
+    createCanvas(w, w)
+    stroke(255, 96)
+}
+
+function draw() {
+    background(9)
+    t += PI/240
+    for (x = 0; x < 10000; x++) {
+        y = x / 235
+        k = (4 + sin(x / 11 + t * 8)) * cos(x / 14)
+        e =  y / 8 - 19
+        d = mag(k, e) + sin(y / 9 + t * 2)
+        q = 2 * sin(k * 2)
+            + sin(y / 17) * k * (9 + 2 * sin(y - d * 3))
+        c = d * d / 49 - t
+        u = q + 50 * cos(c) + 200
+        v = q * sin(c) + d * 39 - 440
+        point(u, v)
     }
 }
