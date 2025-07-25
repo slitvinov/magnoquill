9d3fe53b0fa626d50273a83b439da5d504a0f57c
Sergey Litvinov <slitvinov@gmail.com>
2025-07-25 21:34:52 +0200

    up

--- 3d.py
+++ 3d.py
@@ -1,19 +1,12 @@
 from math import sin, cos, pi, hypot as mag
 
-w = 400
-t = 0
-for i in range(480):
-    canvas = w * w * [0]
-    t += pi / 240
-    for x in range(10000):
-        y = x / 235
-        k = (4 + sin(x / 11 + t * 8)) * cos(x / 14)
-        e = y / 8 - 19
-        d = mag(k, e) + sin(y / 9 + t * 2)
-        q = 2 * sin(k * 2) + sin(y / 17) * k * (9 + 2 * sin(y - d * 3))
-        c = d * d / 49 - t
-        u = q + 50 * cos(c) + 200
-        v = q * sin(c) + d * 39 - 440
-        u = int(u)
-        v = int(v)
-        canvas[u + v * w] += 96
+for x in range(10000):
+    y = x / 235
+    k = (4 + sin(x / 11)) * cos(x / 14)
+    e = y / 8 - 19
+    d = mag(k, e) + sin(y / 9)
+    q = 2 * sin(k * 2) + sin(y / 17) * k * (9 + 2 * sin(y - d * 3))
+    c = d * d / 49
+    u = q + 50 * cos(c)
+    v = q * sin(c) + d * 39
+    print(u, v)
