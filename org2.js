f4007246d76314208ab9ee7b26693079efce0f94
Sergey Litvinov <slitvinov@gmail.com>
2025-07-26 23:25:53 +0200

    org2

---
+++ org2.js
@@ -0,0 +1,2 @@
+a=(x,y,d=mag(k=(4+cos(x/9-t))*cos(x/30),e=y/7-13)+sin(y/99+t/2)-4)=>point((q=3*sin(k*2)+sin(y/29)*k*(9+2*sin(cos(e)*9-d*4+t)))+40*cos(c=d-t)+200,q*sin(c)+d*35)
+t=0,draw=$=>{t||createCanvas(w=400,w);background(9).stroke(w,96);for(t+=PI/120,i=1e4;i--;)a(i,i/235)}
