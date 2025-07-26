d6df290391be4828a649264f10dc66404e796ffa
Sergey Litvinov <slitvinov@gmail.com>
2025-07-26 13:24:59 +0200

    add 3D

--- 3d.py
+++ 3d.py
@@ -1,12 +1,24 @@
 from math import sin, cos, pi, hypot as mag
 
-for x in range(10000):
-    y = x / 235
-    k = (4 + sin(x / 11)) * cos(x / 14)
-    e = y / 8 - 19
-    d = mag(k, e) + sin(y / 9)
-    q = 2 * sin(k * 2) + sin(y / 17) * k * (9 + 2 * sin(y - d * 3))
-    c = d * d / 49
-    u = q + 50 * cos(c)
-    v = q * sin(c) + d * 39
-    print(u, v)
+t = 0
+n = 10000
+for i in range(10):
+    with open("%03d.skel" % i, "w") as file:
+        file.write("SKEL\n")
+        file.write("%d 1\n" % n)
+        for x in range(n):
+            # t += pi / 240
+            y = x / 235
+            k = (4 + sin(x / 11)) * cos(x / 14)
+            e = y / 8 - 19
+            d = mag(k, e) + sin(y / 9)
+            q = 2 * sin(k * 2) + sin(y / 17) * k * (9 + 2 * sin(y - d * 3))
+            c = d * d / 49
+            u = 50 * cos(c) + q
+            v = d * 39 + q * sin(c)
+            w = 30 * sin(c / 2 + 0.1 * q)
+            file.write("%.16e %.16e %.16e\n" % (u, v, w))
+        file.write("%d " % n)
+        for x in range(n):
+            file.write(" %d" % x)
+        file.write(" 0\n")
